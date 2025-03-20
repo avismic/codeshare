d80ff78d3b2528e704c0c5a675af501fedea2819
Abhishek Anand <118845185+avismic@users.noreply.github.com>
2025-03-20 08:02:22 +0530

    Update save.js

--- api/save.js
+++ api/save.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
     const { data, error } = await supabase
       .from('snippets')
       .insert([{ name, code, language }])
+      .select()
       .single();
 
     if (error) {
